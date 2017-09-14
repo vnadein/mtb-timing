342fd88d502d5447da1e77f07f0826c10fcff4cd
line-master <rl@lockerbox.ru>
2017-09-14 06:26:18 +0300

    er

--- scaner.py
+++ scaner.py
@@ -26,32 +26,33 @@ def becon_tracking(ibecon):
         while True:
                 for beacon in scanner.scan():
                         beaco = beacon.split(",")
-                        if beaco[3] == ibecon:
-                                if (beaco[2] == major) and (beaco[3] in minor):
-                                        if i == 3:
-                                                distance.sort()
-                                                print(minor[beaco[3]], ": ", distance)
-                                                if distance[0] < 1:
-                                                        race_time = float(time())
-                                                        print(minor[beaco[3]], ": ", race_time)
-                                                        return True
-                                                distance = []
-                                                i = 1
+                        if beaco[3] == ibecon and ((beaco[2] == major) and (beaco[3] in minor)):
+                                if i == 3:
+                                        distance.sort()
+                                        print('\n[sys] racer ', minor[beaco[3]], ' distance', ": ", distance)
+                                        if distance[0] < 1:
+                                                race_time = float(time())
+                                                print('\n[sys] racer ', minor[beaco[3]], ' finished  -time', ": ", race_time, '\n')
+                                                return True
+                                        distance = []
+                                        i = 1
+                                else:
+                                        rat = ratio(beaco[5], beaco[4])
+                                        if rat < 1:
+                                                i += 1
+                                                dist = math.pow(rat, 10)
+                                                distance.append(round(dist, 2))
                                         else:
-                                                rat = ratio(beaco[5], beaco[4])
-                                                if rat < 1:
-                                                        i += 1
-                                                        dist = math.pow(rat, 10)
-                                                        distance.append(round(dist, 2))
-                                                else:
-                                                        i += 1
-                                                        dist = 0.89976 * math.pow(rat, 7.7095) + 0.111
-                                                        distance.append(round(dist, 2))
+                                                i += 1
+                                                dist = 0.89976 * math.pow(rat, 7.7095) + 0.111
+                                                distance.append(round(dist, 2))
 
 while True:
     for beacon in scanner.scan():
         beaco = beacon.split(",")
+        print('beaco', beaco[3])
+        print('startunit', startUnit)
         if beaco[3] not in startUnit:
                 if (beaco[2] == major) and (beaco[3] in minor):
                         startUnit.append(beaco[3])
-                        thread = Thread(becon_tracking, (beaco[3],)).start()
+                        thread = Thread(becon_tracking(beaco[3])).start()
