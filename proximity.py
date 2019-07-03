372edbe959453151ecb1eac637429a0e77d1ac5b
vnadein <diablo-vn@yandex.ru>
2019-07-03 12:22:16 +0300

    Add files via upload

---
+++ proximity.py
@@ -0,0 +1,22 @@
+import blescan
+import bluetooth._bluetooth as bluez
+
+
+class Scanner:
+    def __init__(self, deviceId=0, loops = 1):
+        self.deviceId = deviceId
+        self.loops = loops
+        try:
+            self.sock = bluez.hci_open_dev(self.deviceId)
+            blescan.hci_le_set_scan_parameters(self.sock)
+            blescan.hci_enable_le_scan(self.sock)
+        except Exception as e:
+            print(e)
+
+    def scan(self):
+        return blescan.parse_events(self.sock, self.loops)
+
+    def test(self):
+        while True:
+            for beacon in self.scan():
+                print(beacon)
